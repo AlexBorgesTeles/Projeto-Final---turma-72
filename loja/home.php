593240095287e431f7672e31e738b9259d326afb
Alex Puro Cálcio Teles <99930474+AlexBorgesTeles@users.noreply.github.com>
2022-05-11 20:30:08 -0300

    Uptade home.php

--- loja/home.php
+++ loja/home.php
@@ -6,6 +6,9 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
     <style>
+        .fonteVerd{
+            font-family:verdana;
+        }
       .bd-placeholder-img {
         font-size: 1.125rem;
         text-anchor: middle;
@@ -26,18 +29,18 @@
             <a class="py-2" href="#" aria-label="Product">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="d-block mx-auto" role="img" viewBox="0 0 24 24"><title>Product</title><circle cx="12" cy="12" r="10"/><path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"/></svg>
             </a>
-            <a class="py-2 d-none d-md-inline-block" href="#">Masculino</a>
-            <a class="py-2 d-none d-md-inline-block" href="#">Feminino</a>
-            <a class="py-2 d-none d-md-inline-block" href="#">Infantil</a>
-            <a class="py-2 d-none d-md-inline-block" href="#">Estoque</a>
-            <a class="py-2 d-none d-md-inline-block" href="#">Ajuda</a>
-            <a class="py-2 d-none d-md-inline-block" href="#">Carrinho</a>
+            <a class=" fonteVerd py-2 d-none d-md-inline-block" href="#" >Masculino</a>
+            <a class=" fonteVerd py-2 d-none d-md-inline-block" href="#">Feminino</a>
+            <a class=" fonteVerd py-2 d-none d-md-inline-block" href="#">Infantil</a>
+            <a class=" fonteVerd py-2 d-none d-md-inline-block" href="#">Estoque</a>
+            <a class=" fonteVerd py-2 d-none d-md-inline-block" href="#">Ajuda</a>
+            <a class=" fonteVerd py-2 d-none d-md-inline-block" href="#">Carrinho</a>
                 <div id="divBusca">
                 <input type="text" id="txtBusca" placeholder="Buscar..."/>
                 <a href="https://www.google.com"><img src="../imagens/Lupa.png" id="btnBusca" alt="Buscar" width="24" height="24"/></a>
                 </div>
                 <img src="../imagens/Login.jpg" id="Login" alt="Login" width="24" height="24"/>
-                <a class="py-2 d-none d-md-inline-block" href="#">Cadastra-se</a>
+                <a class=" fonteVerd py-2 d-none d-md-inline-block" href="#">Cadastra-se</a>
         </nav>
     </header>
     <div class="container">
@@ -47,7 +50,7 @@
         <!---- Fim Linha-->
         <!----Slide Central-->
         <div class="row">
-            <div class="col btn">
+            <div class="col-12 btn">
                 <img src="../imagens/camisetas1.jpg" id="Slide1" alt="Slide1"/>
             </div>
         </div>
@@ -62,8 +65,8 @@
         <!---- Fim Linha-->
         <div class="row">
             <div class="col-3 btn">
-                <div class="card" style="width: 18rem;">
-                        <img src="../imagens/camisas2.jpg" class="card-img-top" alt="camisasC">
+                <div class="card" >
+                        <img src="../imagens/camisas2.jpg" class="card-img-top" alt="camisasC" style="height: 250px">
                     <div class="card-body">
                         <h5 class="card-title">Marca e Estampa</h5>
                         <p class="card-text">R$00,00</p>
@@ -72,8 +75,8 @@
                 </div>
             </div>
             <div class="col-3 btn">
-                <div class="card" style="width: 18rem;">
-                        <img src="../imagens/camisas3.jpg" class="card-img-top" alt="camisasC">
+                <div class="card" >
+                        <img src="../imagens/camisas3.jpg" class="card-img-top" alt="camisasC" style="height: 250px">
                     <div class="card-body">
                         <h5 class="card-title">Marca e Estampa</h5>
                         <p class="card-text">R$00,00</p>
@@ -82,8 +85,8 @@
                 </div>
             </div>
             <div class="col-3 btn">
-                <div class="card" style="width: 18rem;">
-                        <img src="../imagens/camisas2.jpg" class="card-img-top" alt="camisasC">
+                <div class="card" >
+                        <img src="../imagens/camisas2.jpg" class="card-img-top" alt="camisasC" style="height: 250px">
                     <div class="card-body">
                         <h5 class="card-title">Marca e Estampa</h5>
                         <p class="card-text">R$00,00</p>
@@ -92,8 +95,8 @@
                 </div>
             </div>
             <div class="col-3 btn">
-                <div class="card" style="width: 18rem;">
-                        <img src="../imagens/camisas3.jpg" class="card-img-top" alt="camisasC">
+                <div class="card" >
+                        <img src="../imagens/camisas3.jpg" class="card-img-top" alt="camisasC" style="height: 250px">
                     <div class="card-body">
                         <h5 class="card-title">Marca e Estampa</h5>
                         <p class="card-text">R$00,00</p>
@@ -112,13 +115,13 @@
                 </div>
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img src="../imagens/camisas1.jpg" style="height: 500px; width: 120" class="d-block w-100" alt="carrosel1">
+                        <img src="../imagens/camisetas4.jpg" style="height: 500px; width: 120" class="d-block w-100" alt="carrosel1">
                     </div>
                     <div class="carousel-item">
-                        <img src="../imagens/camisas2.jpg" style="height: 500px; width: 120" class="d-block w-100" alt="carrosel2">
+                        <img src="../imagens/camisetas2.jpg" style="height: 500px; width: 120" class="d-block w-100" alt="carrosel2">
                     </div>
                     <div class="carousel-item">
-                        <img src="../imagens/camisas3.jpg" style="height: 500px; width: 120" class="d-block w-100" alt="carrosel3">
+                        <img src="../imagens/camisetas3.jpg" style="height: 500px; width: 120" class="d-block w-100" alt="carrosel3">
                     </div>
                 </div>
                 <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
