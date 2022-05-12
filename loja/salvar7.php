6245f860190d78f6a9cdc8a2a0108f9455f3b35f
yasgalvao999 <99930600+yasgalvao999@users.noreply.github.com>
2022-05-11 21:08:28 -0300

    coment

---
+++ loja/salvar7.php
@@ -0,0 +1,28 @@
+<?php
+    
+
+    $servidor = 'localhost';
+    $usuario = 'root';
+    $senha = '';
+    $banco = 'yasminbd';
+
+    $connection = mysqli_connect($servidor,$usuario,$senha,$banco);
+    $sql="insert into registro(nome, email, senha) VALUES ('{$_POST['nome']}','{$_POST['email']}','{$_POST['senha']}')";
+ 
+
+    $insert = mysqli_query($connection , $sql);
+
+    if($insert){
+        echo "<h2>Login Realizado</h2>";
+    }
+
+    
+#echo $sql;
+
+#id de identificador
+#CRUD
+#Create
+#Read
+#Update
+#Delete
+?>
