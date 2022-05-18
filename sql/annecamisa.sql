a574e0f2e8fdd450ab45a2e469464fd7d95ce14f
Drugs <israelmadrugaps@gmail.com>
2022-05-18 20:52:18 -0300

    Update annecamisa.sql

--- sql/annecamisa.sql
+++ sql/annecamisa.sql
@@ -19,16 +19,16 @@ CREATE  TABLE IF NOT EXISTS `camisa` (
 --
 
 INSERT INTO `camisa` (`estampa`, `marca`, `imagem`, `descricao`, `avaliacao`, `preco`) VALUES
-( 'Harvard', 'Magazine Luiza', 'harvard.jpg', 'Vá para qualquer lugar e se sinta 100% confortável com a camisa da Harvard', '5.00', '189,66'),
-( 'Camisa do Plmeiras', 'PUMA Brasil', 'palmeirasverde.jpg', 'Não deixe mais ninguém te zuar dizendo que o Palmeiras não tem mundial. Yse a camisa e prove o contrário', '2.00', '299,90'),
-( 'Super-man', 'C&A', 'supermam.jpg', '', '4.00', '36,99'),
-( 'The Batman', 'DC Comics', 'batman.jpg', '', '4.00', '49,90'),
-( 'Formiga', 'Netshoes', 'ant.jpg', 'Vista-se e assuste as pessoas', '4.00', '39,90'),
-( 'Foco, Força e Café', 'Limits', 'ffc.jpg', '', '5.00', '101,40'),
-( 'Natureza', 'Havaianas', 'natureza.jpg', '', '2.00', '72,99'),
-( 'Doguinho sorrindo', 'Guten', 'dogsmile.jpg', '', '5.00', '89,90'),
-( 'Harry Potter', 'Riachuelo', 'bruxos.jpg', '', '5.00', '49,90'),
-('Bob esponja', 'Renner', 'bob.jpg', '', '3.00', '29,50');
+( 'Harvard', 'Magazine Luiza', 'harvard.jpg', 'Vá para qualquer lugar e se sinta 100% confortável com a camisa da Harvard', '5.00', '189.66'),
+( 'Camisa do Plmeiras', 'PUMA Brasil', 'palmeirasverde.jpg', 'Não deixe mais ninguém te zuar dizendo que o Palmeiras não tem mundial. Yse a camisa e prove o contrário', '2.00', '299.90'),
+( 'Super-man', 'C&A', 'supermam.jpg', '', '4.00', '36.99'),
+( 'The Batman', 'DC Comics', 'batman.jpg', '', '4.00', '49.90'),
+( 'Formiga', 'Netshoes', 'ant.jpg', 'Vista-se e assuste as pessoas', '4.00', '39.90'),
+( 'Foco, Força e Café', 'Limits', 'ffc.jpg', '', '5.00', '101.40'),
+( 'Natureza', 'Havaianas', 'natureza.jpg', '', '2.00', '72.99'),
+( 'Doguinho sorrindo', 'Guten', 'dogsmile.jpg', '', '5.00', '89.90'),
+( 'Harry Potter', 'Riachuelo', 'bruxos.jpg', '', '5.00', '49.90'),
+('Bob esponja', 'Renner', 'bob.jpg', '', '3.00', '29.50');
 
 --
 -- Índices para tabelas despejadas
