afcd5c4e66a7e0f573cd14193aaa4315f269f834
Megelado <99930585+Megelado@users.noreply.github.com>
2022-05-18 22:37:03 -0300

    Update carrinho.php

--- loja/carrinho.php
+++ loja/carrinho.php
@@ -14,7 +14,43 @@
 	    <div class="col-2">
 	        <div class="card">
       <img src="../imagens/<?php echo $q['imagem']?>" alt="#" height="150px" width="200">
-       <p><?php echo $q['estampa']?></p>
+       <p><?php echo $q['<?php
+$titlePage = "Página exemplo";
+include "../modelos/cabecalho.php";
+include "../modelos/conexao.php";
+$id = $_GET['id_camisa'];
+var_dump($id);
+$sql2 = "select * from `carrinho` join camisa where id_camisa = "."{$id}". "";
+$query2 = mysqli_query($mysql,$sql2);
+$dados = mysqli_fetch_all($query2,MYSQLI_ASSOC);
+foreach($dados as $q)
+?>
+<div class='container'>
+	<div class='row'>
+	    <div class='col-2'>
+	        <div class='card'>
+      <img src='../imagens/<?php echo $q['imagem'];?>' alt='#' height='150px' width='200'>
+       <p><?php echo $q['estampa'];?></p>
+       <p><?php echo $q['tamanho'];?></p>
+       <p><?php echo $q['cor'];?></p>
+       <p>quantidade: <?php echo $q['quantidade'];?></p>
+       <p>R$<?php echo $q['preco'];?></p>
+       <p>data da compra: <?php echo $q['data'];?></p>
+       <p><?php echo $q['avaliacao'];?></p>
+       <a href='' class='btn btn-danger'>Remover produto</a>
+      </div>
+	  </div>
+	  </div>
+	  <br>
+	<div class='row'>
+	    <a href='detalhes.php' class='btn btn-warning'> Continuar compra</a>
+	    <a href='home.php' class='btn btn-primary'> Finalizar compra</a>
+	</div>
+</div>
+<?php
+	include "../modelos/footer.php";
+?>
+estampa']?></p>
        <p><?php echo $q['tamanho']?></p>
        <p><?php echo $q['cor']?></p>
        <p>quantidade no estoque: <?php echo $q['quantidade']?></p>
