14da7bf991d4f25f19ed19bf9785690f450ee58e
Megelado <99930585+Megelado@users.noreply.github.com>
2022-05-20 10:50:24 -0300

    Update cabecalho.php

--- modelos/cabecalho.php
+++ modelos/cabecalho.php
@@ -1,3 +1,4 @@
+  
 <!DOCTYPE HTML>
     <html lang="pt-BR">
     <head>
@@ -5,33 +6,52 @@
         <title><?php echo $titlePage;?></title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+        <!-- Favicon -->
         <!-- <link rel="icon" type="image/png" href="icones/fofo.png"> -->
     </head>
 
-<header class="p-3 bg-dark text-white">
-    <div class="container">
-      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-          <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
-        </a>
+    <header class="site-header sticky-top py-1" style="background-color: gold">
+        <nav class="container d-flex flex-column flex-md-row justify-content-between">
+            <a class="py-2" href="#" aria-label="Product">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="d-block mx-auto" role="img" viewBox="0 0 24 24"><title>Product</title><circle cx="12" cy="12" r="10"/><path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"/></svg>
+            </a>
+            <a class="py-2 d-none d-md-inline-block text-black" href="home.php">Início</a>
+            <a class="py-2 d-none d-md-inline-block text-black" href="#">Masculino</a>
+            <a class="py-2 d-none d-md-inline-block text-black" href="#">Feminino</a>
+            <a class="py-2 d-none d-md-inline-block text-black" href="#">Infantil</a>
+            <a class="py-2 d-none d-md-inline-block text-black" href="#">Estoque</a>
+            <a class="py-2 d-none d-md-inline-block text-black" href="carrinho.php">Carrinho</a>
+                <a href="login.php" class="d-inline p-2  text-black">
+                    Login <i class="bi bi-door-open-fill"></i>
+                </a>
+                <a class="d-inline p-2 text-black" href="cadastro.php">Cadastra-se</a>
+        </nav>
+    </header>  
+<!DOCTYPE HTML>
+    <html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <title><?php echo $titlePage;?></title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+        <!-- Favicon -->
+        <!-- <link rel="icon" type="image/png" href="icones/fofo.png"> -->
+    </head>
 
-        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
-          <li><a href="#" class="nav-link px-2 text-white">Features</a></li>
-          <li><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
-          <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
-          <li><a href="#" class="nav-link px-2 text-white">About</a></li>
-        </ul>
-
-        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
-          <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
-        </form>
-
-        <div class="text-end">
-          <a class="btn btn-outline-light me-2" href="login.php" role="button">Link</a>
-          <a class="btn btn-warning" href="registro.php" role="button">Sign-up</a>
-        </div>
-      </div>
-    </div>
-</header>
-<body>
+    <header class="site-header sticky-top py-1" style="background-color: gold">
+        <nav class="container d-flex flex-column flex-md-row justify-content-between">
+            <a class="py-2" href="#" aria-label="Product">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="d-block mx-auto" role="img" viewBox="0 0 24 24"><title>Product</title><circle cx="12" cy="12" r="10"/><path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"/></svg>
+            </a>
+            <a class="py-2 d-none d-md-inline-block text-black" href="home.php">Início</a>
+            <a class="py-2 d-none d-md-inline-block text-black" href="#">Masculino</a>
+            <a class="py-2 d-none d-md-inline-block text-black" href="#">Feminino</a>
+            <a class="py-2 d-none d-md-inline-block text-black" href="#">Infantil</a>
+            <a class="py-2 d-none d-md-inline-block text-black" href="#">Estoque</a>
+            <a class="py-2 d-none d-md-inline-block text-black" href="carrinho.php">Carrinho</a>
+                <a href="login.php" class="d-inline p-2  text-black">
+                    Login <i class="bi bi-door-open-fill"></i>
+                </a>
+                <a class="d-inline p-2 text-black" href="cadastro.php">Cadastra-se</a>
+        </nav>
+    </header>
