b91e8d5931e87912dcc1b92ed48de0ba8d31b3a3
Drugs <israelmadrugaps@gmail.com>
2022-05-20 20:40:04 -0300

    Update adicionaraocarrinho.php

--- loja/adicionaraocarrinho.php
+++ loja/adicionaraocarrinho.php
@@ -1,9 +1,13 @@
 <?php
 $titlePage = "Página exemplo";
-include "../modelos/cabecalho.php";
-include "../modelos/conexao.php";
+include "cabecalho.php";
+include "conexao.php";
 session_start();
 $select = "select * from carrinho join estoque";
+
+//Add pedido com os dados do usuario/data/ e status em andamento
+//pegar o id do pedido - https://www.w3schools.com/php/php_mysql_insert_lastid.asp > $last_id = mysqli_insert_id($conn);
+//pegar o id do estoque, qual é a cor da camisa?, tem no estoque?
 $sql = "insert into `carrinho` (`fk_id_pessoa`, `fk_id_estoque`, `quantidade`, `fk_id_pedido`) VALUES ('{$_SESSION['user_id']}','{$_SESSION['estoque_id']}','2','{$_SESSION['pedido']}')"; 
 $query = mysqli_query($mysql,$sql);
 $dados = mysqli_fetch_all($query,MYSQLI_ASSOC);
