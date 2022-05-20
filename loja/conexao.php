b9673dfce8e08529a568b94aff174795de21c87a
Drugs <israelmadrugaps@gmail.com>
2022-05-20 19:14:35 -0300

    Update conexao.php

--- loja/conexao.php
+++ loja/conexao.php
@@ -4,7 +4,7 @@
 	$senha = '';
 	$banco = 'bancoloja';
 
-	$connection = mysqli_connect($servidor, $usuario, $senha, $banco);
+	$mysql = mysqli_connect($servidor, $usuario, $senha, $banco);
 
 	//já conectou, se tudo estiver certo.
 
