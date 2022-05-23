3384fdd66ef41733a7042d9ad8b234f76d33048d
yasgalvao999 <99930600+yasgalvao999@users.noreply.github.com>
2022-05-23 15:22:58 -0300

    confirmacadastro

--- loja/confirmacadastro.php
+++ loja/confirmacadastro.php
@@ -4,13 +4,12 @@ $sql="insert into pessoa(nome, cpf, rg, endereco, idade, telefone) VALUES ('{$_P
 $sql="insert into usuario(email,senha,nivel) VALUES ('{$_POST['email']}','{$_POST['senha']}','{$_POST['nivel']}')";	
 $query=mysqli_query($mysql,$sql);
 $dados=mysqli_fecth_assoc($query);
-
-#var_dump($dados);  
+#var_dump($dados);
+if(isset( if($_POST['nome'] and $_POST['cpf'] and $_POST['rg'] and $_POST['endereco'] and $_POST['idade'] and $_POST['telefone'] and $_POST['email'] and $_POST['senha']){
+  echo "Incompleto";
+}
  
-   
-            
-    $insert = mysqli_query($mysql , $sql);
-
+  
     if($insert){
     
         header("Location: home.php");
