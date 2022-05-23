384011efacc7a969aad85b6d383bf3efb28911a2
yasgalvao999 <99930600+yasgalvao999@users.noreply.github.com>
2022-05-23 16:03:34 -0300

    confirmacadastro

--- loja/confirmacadastro.php
+++ loja/confirmacadastro.php
@@ -1,11 +1,11 @@
 <?php
 include 'conexao.php';
-$sql="insert into pessoa(nome, cpf, rg, endereco, idade, telefone) VALUES ('{$_POST['nome']}','{$_POST['cpf']}','{$_POST['rg']}','{$_POST['ender']}','{$_POST['idade']}','{$_POST['tel']}')";
-$sql="insert into usuario(email,senha,nivel) VALUES ('{$_POST['email']}','{$_POST['senha']}','{$_POST['nivel']}')";	
-$query=mysqli_query($mysql,$sql);
+$sqlpessoa="insert into pessoa(nome, cpf, rg, endereco, idade, telefone) VALUES ('{$_POST['nome']}','{$_POST['cpf']}','{$_POST['rg']}','{$_POST['ender']}','{$_POST['idade']}','{$_POST['tel']}')";
+$sqlusuario="insert into usuario(email,senha,nivel) VALUES ('{$_POST['email']}','{$_POST['senha']}','{$_POST['nivel']}')";	
+$query=mysqli_query($mysql,$sqlpessoa,$sqlusuario);
 $dados=mysqli_fecth_assoc($query);
 #var_dump($dados);
-if(isset( if($_POST['nome'] and $_POST['cpf'] and $_POST['rg'] and $_POST['endereco'] and $_POST['idade'] and $_POST['telefone'] and $_POST['email'] and $_POST['senha']){
+if(isset($_POST['nome'] and $_POST['cpf'] and $_POST['rg'] and $_POST['endereco'] and $_POST['idade'] and $_POST['telefone'] and $_POST['email'] and $_POST['senha']){
   echo "Incompleto";
 }
  
