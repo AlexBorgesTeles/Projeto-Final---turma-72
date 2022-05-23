68fa53b85e6bc7b537eb029fabf824f59c80c817
yasgalvao999 <99930600+yasgalvao999@users.noreply.github.com>
2022-05-23 19:30:25 -0300

    confirmacadastro

--- loja/confirmacadastro.php
+++ loja/confirmacadastro.php
@@ -6,8 +6,7 @@ $query=mysqli_query($mysql,$sqlpessoa,$sqlusuario);
 $dados=mysqli_fecth_assoc($query);
 #var_dump($dados);
 if(isset($_POST['nome'] and $_POST['cpf'] and $_POST['rg'] and $_POST['endereco'] and $_POST['idade'] and $_POST['telefone'] and $_POST['email'] and $_POST['senha']){
-  
-echo "Cadastro realizado com sucesso";
+  echo "Cadastro realizado com sucesso";
 }
   
     if($insert){
