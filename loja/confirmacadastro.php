5be5d47692a53710fdc6d061ae33dd197f86f060
yasgalvao999 <99930600+yasgalvao999@users.noreply.github.com>
2022-05-24 20:26:55 -0300

    confirmacadastro

--- loja/confirmacadastro.php
+++ loja/confirmacadastro.php
@@ -1,6 +1,6 @@
 <?php
 include 'conexao.php';
-$sqlpessoa="INSERT INTO `pessoa`(`id_pessoa`, `nome`, `cpf`, `rg`, `endereco`, `idade`, `telefone`) VALUES ('{$_POST['id_pessoa']} ','{$_POST['nome']}','{$_POST['cpf']}','{$_POST['rg']}','{$_POST['endereco']}','{$_POST['idade']','{$_POST['telefone']}')";
+$sqlpessoa="INSERT INTO `pessoa`(`id_pessoa`, `nome`, `cpf`, `rg`, `endereco`, `idade`, `telefone`) VALUES ('{$_POST['id_pessoa']} ','{$_POST['nome']}','{$_POST['cpf']}','{$_POST['rg']}','{$_POST['endereco']}','{$_POST['idade']}','{$_POST['telefone']}')";
 $sqlusuario="INSERT INTO `usuario`(`id_usuario`, `email`, `senha`, `nivel`) VALUES ('{$_POST['id_usuario']}','{$_POST['email']}','{$_POST['senha']}','2";	
 $query=mysqli_query($mysql,$sqlpessoa,$sqlusuario);
 
