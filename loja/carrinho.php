73cf05590bfb2c497201311c0e94e4cc7f6acdf5
Megelado <99930585+Megelado@users.noreply.github.com>
2022-05-25 09:49:08 -0300

    Update carrinho.php

--- loja/carrinho.php
+++ loja/carrinho.php
@@ -1,36 +1,45 @@
 <?php
+include "conexao.php";
+session_start();
 $titlePage = "Página exemplo";
 include "cabecalho.php";
-include "conexao.php";
-$id = $_GET['id_camisa'];
-$sql = "select * from `carrinho` join estoque on estoque.fk_id_camisa = {$id} join camisa on camisa.id_camisa = {$id}";
+if(isset($_SESSION['user_id']) and $_SESSION['user_id'] != ''){
+		$sql2 = "select * from pessoa where id_pessoa = ".$_SESSION['user_id'];
+		$query = mysqli_query($mysql,$sql2);
+		$pessoa = mysqli_fetch_assoc($query);
+		$nome = $pessoa['nome'];
+	}
+
+$id = $_GET['camisa'];
+$sql = "SELECT * FROM carrinho JOIN estoque ON carrinho.fk_id_pessoa = {$_SESSION['user_id']} join camisa on estoque.id_estoque = carrinho.fk_id_estoque WHERE id_camisa = estoque.fk_id_camisa;";
 $query = mysqli_query($mysql,$sql);
 $dados = mysqli_fetch_all($query,MYSQLI_ASSOC);
+var_dump($dados);
 foreach($dados as $q)
 ?>
 <div class="container">
        <?php
               echo "
-                     <div class='card'>
-	                     <img src='../imagens/{$q['imagem']}' class='card-img-top'alt='#' height='150px' width='200'>
-                            <p class='card-title'>
-                            {$q['estampa']}</p>
-                            <div class='card-text'>
-                            <p>{$q['tamanho']}</p>
-                            <p>{$q['cor']}</p>
+                     <div class='card' style='background-color: blue'>
+	                     <img src='../imagens/{$q['imagem']}' class='card-img-top'alt='#' style='height: 110px; width:160px;'>
+                            <div class='card-body'>
+                            <h4>{$q['estampa']}</h4></div>
+                            <h6 class='card-title'>tamanho disponivel: 
+                            {$q['tamanho']}</h6>
+                            <p>cor disponivel: {$q['cor']}</p>
                             <p> quantidade requisitada: {$q['quantidade']}</p>
                             <p>quantidade no estoque: {$q['quantidade_e']}</p>
-                            <p>R$ {$q['preco']}</p></div><p class='card-text>'";   
+                            <p>R$ {$q['preco']}</p><p>'";   
                             $contador=0;
                             while($contador < 5){
                                    if($dados[0]['avaliacao'] > $contador){echo "<i class='bi bi-star-fill'></i>";}if($dados[0]['avaliacao'] < $contador){echo "<i class='bi bi-star'></i>";}
                             $contador++;}
-                            echo "</p><p><a href='' class='btn btn-danger'>Remover produto</a></p>
+                            echo "</p></div><p><a href='deleteproduto.php?del=$dados[0]['id_carrinho']' class='btn btn-danger'>Remover produto</a></p>
                      </div>
               ";
        ?>
      	<div class='row'>
-	     <a href='detalhes.php' class='btn btn-warning'> Continuar compra</a>
+	     <a href='detalhes.php' class='btn btn-dark'> Continuar compra</a>
 	     <br>
 	     <a href='fimdecompra.php' class='btn btn-primary'> Finalizar compra</a>
        </div>
