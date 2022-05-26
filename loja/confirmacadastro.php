2ad76720c1ffae985d3bbdf1eb3b6c7c43b4f4a4
yasgalvao999 <99930600+yasgalvao999@users.noreply.github.com>
2022-05-25 21:31:23 -0300

    confirmacadastro

--- loja/confirmacadastro.php
+++ loja/confirmacadastro.php
@@ -3,16 +3,11 @@ include 'conexao.php';
 $sqlpessoa="INSERT INTO `pessoa`(`id_pessoa`, `nome`, `cpf`, `rg`, `endereco`, `idade`, `telefone`) VALUES ('{$_POST['id_pessoa']} ','{$_POST['nome']}','{$_POST['cpf']}','{$_POST['rg']}','{$_POST['endereco']}','{$_POST['idade']}','{$_POST['telefone']}')";
 $sqlusuario="INSERT INTO `usuario`(`id_usuario`, `email`, `senha`, `nivel`) VALUES ('{$_POST['id_usuario']}','{$_POST['email']}','{$_POST['senha']}','2";	
 $query=mysqli_query($mysql,$sqlpessoa,$sqlusuario);
-
-
 $dados=mysqli_fecth_assoc($query);
 #var_dump($dados);
-if ($_POST['nome'] and $_POST['cpf'] and $_POST['rg'] and $_POST['endereco'] and $_POST['idade'] and $_POST['telefone'] and $_POST['email'] and $_POST['senha']){
-echo "Cadastro realizado com sucesso";
-header('Location:home.php');
-}else{
-header('Location:cadastro.php');
-}
+
+
+
 
 
   
