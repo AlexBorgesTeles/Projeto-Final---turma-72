60bec1fa34953c2d590df4431d882677c31a0042
Drugs <israelmadrugaps@gmail.com>
2022-05-26 19:55:40 -0300

    Update confirmacadastro.php

--- loja/confirmacadastro.php
+++ loja/confirmacadastro.php
@@ -1,18 +1,18 @@
 <?php
-include 'conexao.php';
-#var_dump($dados);
-if ($_POST['nome'] != '' and $_POST['cpf'] != '' and $_POST['rg'] != '' and $_POST['endereco'] != '' and $_POST['idade'] != '' and $_POST['telefone'] != '' and $_POST['email'] != '' and $_POST['senha'] != ''){
+	//tirar o nivel do cadastro
+	include 'conexao.php';
+	#var_dump($dados);
+	if ($_POST['nome'] != '' and $_POST['cpf'] != '' and $_POST['rg'] != '' and $_POST['endereco'] != '' and $_POST['idade'] != '' and $_POST['telefone'] != '' and $_POST['email'] != '' and $_POST['senha'] != ''){
 	
 	$sqlpessoa="INSERT INTO `pessoa`(`nome`, `cpf`, `rg`, `endereco`, `idade`, `telefone`) VALUES ('{$_POST['nome']}','{$_POST['cpf']}','{$_POST['rg']}','{$_POST['endereco']}','{$_POST['idade']}','{$_POST['telefone']}')";
-	echo $sqlpessoa;
+	$query=mysqli_query($mysql,$sqlpessoa);
+	$last_id = mysqli_insert_id($mysql);
+	echo $last_id;
 	//teste o sqlpessoa no BD
 	//pegue o id 
 	//idpessoa
-	$sqlusuario="INSERT INTO `usuario`(`fk_id_pessoa`, `email`, `senha`, `nivel`) VALUES ('{$sqlpessoa['id_pessoa']}','{$_POST['email']}','{$_POST['senha']}','2";	
-	$query=mysqli_query($mysql,$sqlpessoa,$sqlusuario);
-	$dados=mysqli_fecth_assoc($query);
-	echo "Cadastro realizado com sucesso";
-#header('Location:home.php');
+	$sqlusuario="INSERT INTO `usuario`(`fk_id_pessoa`, `email`, `senha`, `nivel`) VALUES ('{$last_id}','{$_POST['email']}','{$_POST['senha']}','2";	
+	#header('Location:home.php');
 }else{
 #header('Location:cadastro.php');
 }
