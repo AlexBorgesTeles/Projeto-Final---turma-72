51b9e8bc9cebd49bb51e5c560646af62ae2d2a88
yasgalvao999 <99930600+yasgalvao999@users.noreply.github.com>
2022-05-27 19:54:29 -0300

    confirmacadastro

--- loja/confirmacadastro.php
+++ loja/confirmacadastro.php
@@ -12,10 +12,11 @@
 	//pegue o id 
 	//idpessoa
 	$sqlusuario="INSERT INTO `usuario`(`fk_id_pessoa`, `email`, `senha`) VALUES ('{$last_id}','{$_POST['email']}','{$_POST['senha']}')";
-     	echo $sqlusuario;
 	$last_id = mysqli_insert_id($mysql);
         echo $last_id;
 #header('Location:home.php');
+if ($_POST['nome'] and $_POST['cpf'] and $_POST['rg'] and $_POST['endereco'] and $_POST['idade'] and $_POST['telefone'] and $_POST['email'] and $_POST['senha']){
+echo "Cadastro realizado com sucesso";
 }else{
 #header('Location:cadastro.php');
 }
