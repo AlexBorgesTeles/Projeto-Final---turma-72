ebdd8fda3f74fea61f0ab5d06d3db9e1076fed91
Drugs <israelmadrugaps@gmail.com>
2022-05-27 19:28:38 -0300

    Update deleteproduto.php

--- loja/deleteproduto.php
+++ loja/deleteproduto.php
@@ -1,11 +1,12 @@
 <?php
 include "conexao.php";
 session_start();
-if(isset ($_GET['apagar']) and $_GET['apagar']== 4)
-{$sql = "delete from carrinho where id_carrinho={$_GET['delete']}";
-header('Location: carrinho.php');
-mysqli_query($mysql,$sql);
+if(isset ($_GET['apagar']) and $_GET['apagar']== 4){
+	$sql = "delete from carrinho where id_carrinho={$_GET['delete']}";
+	mysqli_query($mysql,$sql);
+	header('Location: carrinho.php?ok=1');
 }
+$titlePage = "Remover Item";
 include "cabecalho.php";
 ?>
 
@@ -13,7 +14,7 @@ include "cabecalho.php";
 
 <div class="container">
 <h2><b>Tem certeza que deseja deletar esse produto do seu carrinho???(?_?)</b> </h2>
-<a class="btn btn-danger"href="deleteproduto.php?apagar=4&del=<? $_GET['del'];?>">sim!</a>
+<a class="btn btn-danger"href="deleteproduto.php?apagar=4&delete=<?= $_GET['delete'];?>">sim!</a>
 <a class="btn btn-primary" href="carrinho.php" role="button">não</a>
 </div>
 
