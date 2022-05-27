8f15e6c1ed11e3eeb2b002e2ad9ec79088f6a6d3
yasgalvao999 <99930600+yasgalvao999@users.noreply.github.com>
2022-05-27 20:20:47 -0300

    luta

--- loja/confirmacadastro.php
+++ loja/confirmacadastro.php
@@ -7,13 +7,13 @@
 	$sqlpessoa="INSERT INTO `pessoa`(`nome`, `cpf`, `rg`, `endereco`, `idade`, `telefone`) VALUES ('{$_POST['nome']}','{$_POST['cpf']}','{$_POST['rg']}','{$_POST['endereco']}','{$_POST['idade']}','{$_POST['telefone']}')";
 	$query=mysqli_query($mysql,$sqlpessoa);
 	$last_id = mysqli_insert_id($mysql);
-	echo $last_id;
+	
 	//teste o sqlpessoa no BD
 	//pegue o id 
 	//idpessoa
 	$sqlusuario="INSERT INTO `usuario`(`fk_id_pessoa`, `email`, `senha`) VALUES ('{$last_id}','{$_POST['email']}','{$_POST['senha']}')";
 	$last_id = mysqli_insert_id($mysql);
-        echo $last_id;
+        
 #header('Location:home.php');
 if ($_POST['nome'] and $_POST['cpf'] and $_POST['rg'] and $_POST['endereco'] and $_POST['idade'] and $_POST['telefone'] and $_POST['email'] and $_POST['senha']){
 echo "Cadastro realizado com sucesso";
