04c53a4c587b45cd34551c86c1965a556fd9b0c9
Drugs <israelmadrugaps@gmail.com>
2022-05-27 20:46:15 -0300

    Update confirmacadastro.php

--- loja/confirmacadastro.php
+++ loja/confirmacadastro.php
@@ -2,25 +2,21 @@
 	//tirar o nivel do cadastro
 	include 'conexao.php';
 	#var_dump($dados);
-	if ($_POST['nome'] != '' and $_POST['cpf'] != '' and $_POST['rg'] != '' and $_POST['endereco'] != '' and $_POST['idade'] != '' and $_POST['telefone'] != '' and $_POST['email'] != '' and $_POST['senha'] != '')
-	
-	$sqlpessoa="INSERT INTO `pessoa`(`nome`, `cpf`, `rg`, `endereco`, `idade`, `telefone`) VALUES ('{$_POST['nome']}','{$_POST['cpf']}','{$_POST['rg']}','{$_POST['endereco']}','{$_POST['idade']}','{$_POST['telefone']}')";
-	$query=mysqli_query($mysql,$sqlpessoa);
-	$last_id = mysqli_insert_id($mysql);
-	
-	//teste o sqlpessoa no BD
-	//pegue o id 
-	//idpessoa
-	$sqlusuario="INSERT INTO `usuario`(`fk_id_pessoa`, `email`, `senha`) VALUES ('{$last_id}','{$_POST['email']}','{$_POST['senha']}')";
-	$last_id = mysqli_insert_id($mysql);
-        
-#header('Location:home.php');
-if ($_POST['nome'] and $_POST['cpf'] and $_POST['rg'] and $_POST['endereco'] and $_POST['idade'] and $_POST['telefone'] and $_POST['email'] and $_POST['senha']){
-echo "Cadastro realizado com sucesso";
-   
-}else{
-#header('Location:cadastro.php');
-}
+	if ($_POST['nome'] != '' and $_POST['cpf'] != '' and $_POST['rg'] != '' and $_POST['endereco'] != '' and $_POST['idade'] != '' and $_POST['telefone'] != '' and $_POST['email'] != '' and $_POST['senha'] != ''){
+		$sqlpessoa="INSERT INTO `pessoa`(`nome`, `cpf`, `rg`, `endereco`, `idade`, `telefone`) VALUES ('{$_POST['nome']}','{$_POST['cpf']}','{$_POST['rg']}','{$_POST['endereco']}','{$_POST['idade']}','{$_POST['telefone']}')";
+		$query=mysqli_query($mysql,$sqlpessoa);
+		$last_id = mysqli_insert_id($mysql);
+		
+		//teste o sqlpessoa no BD
+		//pegue o id 
+		//idpessoa
+		$senha = base64_encode($_POST['senha']);
+		$sqlusuario="INSERT INTO `usuario`(`fk_id_pessoa`, `email`, `senha`) VALUES ('{$last_id}','{$_POST['email']}','{$senha}')";
+		$query=mysqli_query($mysql,$sqlusuario);
+		header('Location:login.php');
+	}else{
+		header('Location:cadastro.php');
+	}
 
 
   
