27b8e21690d98100d7f59abf7e13b67b86409a86
Megelado <99930585+Megelado@users.noreply.github.com>
2022-05-28 13:45:52 -0300

    Update insertcarrinho.php

--- loja/insertcarrinho.php
+++ loja/insertcarrinho.php
@@ -1,46 +1,60 @@
 <?php
-	include "conexao.php";
-	session_start();
-	$titlePage = "Adicionar ao carrinho";
-	include "cabecalho.php";
-	$id = $_GET['id_camisa'];
-        $quantidade = $_GET['quantidade'];
-	#echo $id;
-	if(isset($_SESSION['user_id']) and $_SESSION['user_id'] != ''){
-		$sql2 = "select * from pessoa where id_pessoa = ".$_SESSION['user_id'];
-		$query = mysqli_query($mysql,$sql2);
-		$pessoa = mysqli_fetch_assoc($query);
-		$nome = $pessoa['nome'];
-	}
-	
-	$chamada = "select * from pedido where fk_id_pessoa = {$_SESSION['user_id']}";
-	$Q = mysqli_query($mysql, $chamada);
-	$dados = mysqli_fetch_assoc($Q);
-	
-	//Caso não exista pedido ---
-	if($dados['id_pedido'] == 0){
-		$insert = "insert into `pedido` (`fk_id_pessoa`,`data`,`status`) values('{$_SESSION['user_id']}','".date("Y-m-d")."', 'Em andamento')";
-		$query3 = mysqli_query($mysql,$insert);
-	}
-	//caso pedido exista e esteja fechado:
-	if($dados['id_pedido'] != 0 and $dados['status'] != 'Em andamento'){
-		$insert = "insert into `pedido` (`fk_id_pessoa`,`data`,`status`) values('{$_SESSION['user_id']}','".date("Y-m-d")."', 'Em andamento')";
-$query3 = mysqli_query($mysql,$insert);
-	}
-	//caso o pedido exista e esteja ativo
-$selectpedi = "SELECT * FROM `pedido` WHERE `fk_id_pessoa` = {$_SESSION['user_id']} and `status` = 'Em andamento'";
-$query = mysqli_query($mysql,$selectpedi);
-$pedido = mysqli_fetch_assoc($query);
-	
-	//pegar info de estoque
-	
-$select = "select * from estoque where fk_id_camisa = {$id} and tamanho = {$_GET['tam']}";
-$query4 = mysqli_query($mysql,$select);
-$estoque = mysqli_fetch_assoc($query4);
-	
-if($estoque['quantidade_e'] == 0){echo 'Não há esse produto em nossos estoques!';}else{echo "";}
+include "conexao.php";
+session_start();
+$titlePage = "Carrinho";
+include "cabecalho.php";
+if (isset($_SESSION["user_id"]) and $_SESSION["user_id"] != "") {
+    $sql2 = "select * from pessoa where id_pessoa = " . $_SESSION["user_id"];
+    $query = mysqli_query($mysql, $sql2);
+    $pessoa = mysqli_fetch_assoc($query);
+    $nome = $pessoa["nome"];
+} else {
+    header("Location: login.php?carrinho=1");
+}
+if (isset($_GET["ok"]) and $_GET["ok"] == 1) {
+    echo "<div class='alert alert-success alert-dimissible fade show' role='success'>Produto foi retirado do carrinho!<button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button></div>";
+}
+$sql = "SELECT * FROM carrinho JOIN estoque ON carrinho.fk_id_pessoa = {$_SESSION["user_id"]} join camisa on estoque.id_estoque = carrinho.fk_id_estoque WHERE id_camisa = estoque.fk_id_camisa;";
+$query = mysqli_query($mysql, $sql);
+$dados = mysqli_fetch_all($query, MYSQLI_ASSOC);
+#var_dump($dados);
+echo "<div class='container'><div class='row'>";
+foreach ($dados as $q) {
+    echo "<div class='col-3 btn'><div class='card' style='background-color: #4F4F4F'>
+	                     <img src='../imagens/{$q["imagem"]}' class='card-img-top'alt='#'>
+						<div class='card-body' style='color:white'>
+                            <h4 class='card-title' >{$q["estampa"]}</h4>
+                            <h6 class='card-body'>tamanho: 
+                            {$q["tamanho"]}</h6>
+                            <p class='card-text'>cor: {$q["cor"]}</p>
+                            <p class='card-text'> quantidade: {$q["quantidade"]}</p>
+                            <p class='card-text'>R$ {$q["preco"]}</p>
+
+						
+							
+							<a href='deleteproduto.php?delete={$q["id_carrinho"]}' class='btn btn-danger'>Remover produto</a></div>
+ </div>
+   </div>";
+}
+?>
+     	<div class='row'>
+	     <a id='b1' href='home.php' class='btn btn-warning'> Continuar compra</a>
         
-$insertcarrinho = "insert into `carrinho` (`fk_id_pessoa`,`fk_id_estoque`, `quantidade`, `fk_id_pedido`) values ('{$pessoa['id_pessoa']}','{$estoque['id_estoque']}','{$quantidade}','{$pedido['id_pedido']}')";
-$query = mysqli_query($mysql, $insertcarrinho);
-if($query != 0){header('Location: home.php?sucess=1');}
-	?>
+	     <a id='b2' href='fimdecompra.php' class='btn btn-primary'> Finalizar compra</a>
+        </div>
+        <style>
+            #b1 {
+                margin-left: 100px;
+                width: 40%;
+            }
+
+            #b2 {
+                margin-left: 100px;
+                width: 40%;
+            }
+        </style>
+        
+</div>
+       </div>
+<?php include "footer.php";
+?>
