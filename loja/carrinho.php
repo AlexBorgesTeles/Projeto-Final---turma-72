997cca1437021ffcdbb2212198b03eb16f3e720a
Megelado <99930585+Megelado@users.noreply.github.com>
2022-05-28 11:21:56 -0300

    Update carrinho.php

--- loja/carrinho.php
+++ loja/carrinho.php
@@ -8,7 +8,7 @@ if (isset($_SESSION["user_id"]) and $_SESSION["user_id"] != "") {
     $query = mysqli_query($mysql, $sql2);
     $pessoa = mysqli_fetch_assoc($query);
     $nome = $pessoa["nome"];
-}
+}else{header('Location: login.php?carrinho=1');}
 if(isset($_GET['ok']) and $_GET['ok'] == 1){echo "<div class='alert alert-success alert-dimissible fade show' role='success'>Produto foi retirado do carrinho!<button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button></div>";}
 $sql = "SELECT * FROM carrinho JOIN estoque ON carrinho.fk_id_pessoa = {$_SESSION["user_id"]} join camisa on estoque.id_estoque = carrinho.fk_id_estoque WHERE id_camisa = estoque.fk_id_camisa;";
 $query = mysqli_query($mysql, $sql);
@@ -20,24 +20,12 @@ foreach ($dados as $q) {
 	                     <img src='../imagens/{$q["imagem"]}' class='card-img-top'alt='#' >
 						<div class='card-body' style='color:white'>
                             <h4 class='card-title' >{$q["estampa"]}</h4>
-                            <h6 class='card-body'>tamanho disponivel: 
+                            <h6 class='card-body'>tamanho: 
                             {$q["tamanho"]}</h6>
-                            <p class='card-text'>cor disponivel: {$q["cor"]}</p>
-                            <p class='card-text'> quantidade requisitada: {$q["quantidade"]}</p>
-                            <p class='card-text'>quantidade no estoque: {$q["quantidade_e"]}</p>
+                            <p class='card-text'>cor: {$q["cor"]}</p>
+                            <p class='card-text'> quantidade: {$q["quantidade"]}</p
                             <p class='card-text'>R$ {$q["preco"]}</p>  
-                            <p class='card-text' style='color:white;'>";
-    $contador = 0;
-    while ($contador < 5) {
-        if ($q["avaliacao"] > $contador) {
-            echo "<i class='bi bi-star-fill'></i>";
-        }
-        if ($q["avaliacao"] < $contador) {
-            echo "<i class='bi bi-star'></i>";
-        }
-        $contador++;
-    }
-    echo "</p>
+                
 	<a href='deleteproduto.php?delete={$q['id_carrinho']}' class='btn btn-danger'>Remover produto</a>
 	</div>
 	</div>
