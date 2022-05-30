b0f8581f6502cc90f12d7b31d074e98bccffe6a7
yasgalvao999 <99930600+yasgalvao999@users.noreply.github.com>
2022-05-30 19:57:00 -0300

    lutando

--- loja/confirmacadastro.php
+++ loja/confirmacadastro.php
@@ -17,7 +17,7 @@
 		
 if ($_POST['nome'] and $_POST['cpf'] and $_POST['rg'] and $_POST['endereco'] and $_POST['idade'] and $_POST['telefone'] and $_POST['email'] and $_POST['senha']){
 echo "Cadastro realizado com sucesso";
-header('Location:home.php');
+header('Location:login.php');
 }else{
 
 header('Location:cadastro.php');
