6cefd35af3e999017c01badeca3213b5ed15445d
Megelado <99930585+Megelado@users.noreply.github.com>
2022-05-30 20:45:45 -0300

    Update insertcarrinho.php

--- loja/insertcarrinho.php
+++ loja/insertcarrinho.php
@@ -5,8 +5,6 @@ $titlePage = "Adicionar ao carrinho";
 include "cabecalho.php";
 $id = $_GET["id_camisa"];
 var_dump($id);
-$quantidade = $_GET["quantidade"];
-var_dump($quantidade);
 #echo $id;
 if (isset($_SESSION["user_id"]) and $_SESSION["user_id"] != "") {
     $sql2 = "select * from pessoa where id_pessoa = " . $_SESSION["user_id"];
@@ -24,6 +22,7 @@ if ($dados["id_pedido"] == 0) {
     $insert =
         "insert into `pedido` (`fk_id_pessoa`,`data`,`status`) values('{$_SESSION["user_id"]}','" .
         date("Y-m-d") .
+
         "', 'Em andamento')";
     $query3 = mysqli_query($mysql, $insert);
 }
@@ -55,11 +54,11 @@ if ($estoque["quantidade_e"] == 0) {
 } else {
     echo "";
 }
-// se $quantidade ou $_GET["tam"] forem != ''
-$insertcarrinho = "insert into `carrinho` (`fk_id_pessoa`,`fk_id_estoque`, `quantidade`, `fk_id_pedido`) values ('{$pessoa["id_pessoa"]}','{$estoque["id_estoque"]}','{$quantidade}','{$pedido["id_pedido"]}')";
+// se $_GET['quantidade'] ou $_GET["tam"] forem != ''
+if($_GET['quantidade'] != ''||$_GET["tam"] != ''){
+$insertcarrinho = "insert into `carrinho` (`fk_id_pessoa`,`fk_id_estoque`, `quantidade`, `fk_id_pedido`) values ('{$pessoa["id_pessoa"]}','{$estoque["id_estoque"]}','{$_GET['quantidade']}','{$pedido["id_pedido"]}')";
 $query = mysqli_query($mysql, $insertcarrinho);
-if ($query != 0) {
-    header("Location: home.php?sucess=1");
-}
+header('Location: home.php?sucess=1');
+}else{header('Location: detalhes.php?error=1');}
 //else{header("Location: home.php?error=1");}
 ?>
