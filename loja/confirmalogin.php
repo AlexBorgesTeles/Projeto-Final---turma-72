c298a43261bb5c7a0b450dafc18764da76d31c3b
Drugs <israelmadrugaps@gmail.com>
2022-05-31 19:46:44 -0300

    Cabecalho/home/login

--- loja/confirmalogin.php
+++ loja/confirmalogin.php
@@ -4,15 +4,17 @@
 	$sql = "select * from usuario where email = '{$_POST['email']}'";
 	$query = mysqli_query($mysql,$sql);
 	$dados = mysqli_fetch_assoc($query);
-	$SESSION['id_usuario'] = $dados['id_usario'];
-	$SESSION['email'] = $dados['email'];
-	$SESSION['senha'] = $dados['senha'];
-	$SESSION['nivel'] = $dados['nivel'];
+	
 	
 	//verificar se usuario existe antes do teste
 	#var_dump($dados);
 	if(isset($_POST['email']) and $_POST['email'] == $dados['email']){
 		if(base64_encode($_POST['senha']) == $dados['senha']){
+			//o cara logou
+			$_SESSION['id_usuario'] = $dados['id_usuario'];
+			$_SESSION['email'] = $dados['email'];
+			$_SESSION['senha'] = $dados['senha'];
+			$_SESSION['nivel'] = $dados['nivel'];
 			$_SESSION['user_id'] = $dados['fk_id_pessoa'];
 			header('Location: home.php');
 		}
