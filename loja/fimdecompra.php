dd9bc06fb56feef24146a97bfc98bb41005e2686
canned4 <canne4711@gmail.com>
2022-05-31 21:35:04 -0300

    update fimdecompra.php

--- loja/fimdecompra.php
+++ loja/fimdecompra.php
@@ -1,5 +1,15 @@
 <?php
     session_start();
+    if (isset($_SESSION["user_id"]) and $_SESSION["user_id"] == ""){
+        header('Location: login.php?usuarionaolocalizado=4');
+    }
+	include "conexao.php";
+    if (isset($_SESSION["user_id"]) and $_SESSION["user_id"] != ""){
+        $consulta = "SELECT * FROM pessoa WHERE id_pessoa = ". $_SESSION["user_id"];
+        $busca = mysqli_query($mysql, $consulta);
+        $pessoa = mysqli_fetch_assoc($busca);
+        $nome = $pessoa["nome"];
+    }
     #var_dump($_SESSION);
     include "conexao.php";
     $id = $_GET['id_camisa'];
