cb2dd8fe674952b4f8b5f2f5fb69d3d2f494138c
canned4 <canne4711@gmail.com>
2022-06-03 00:14:06 -0300

    summary

--- loja/checkout.php
+++ loja/checkout.php
@@ -22,7 +22,6 @@
     $pessoa = mysqli_fetch_assoc($pegar);
 	$titlePage = "Pagamento";
 	include "cabecalho.php";
-    include "dadosentrega.php";
 ?>
 <body>
     <div class="container">
@@ -30,58 +29,39 @@
             <h3>Método de pagamento</h3>
             <div class="row">
                 <div class="col flex items-center TvGNLb">
-                    <button id="cartao" class="btn-success">Cartão De Crédito</button>
                     <button id="boleto" class="btn">Boleto Bancário</button>
                     <button id="pix" class="btn">Pix</button>
                 </div>
             </div>
-            
-            </div>
-            <div class="checkout-payment-setting__payment-method-options"></div>
-                <div class="KqH1Px">
-                    <div class="lhwDvd Exv9ow c5Dezq">Subtotal dos Produtos:</div>
-                </div>
+
+            <div class="">
+                <div class="lhwDvd Exv9ow c5Dezq">Subtotal dos Produtos:</div>
                 <div class="lhwDvd Uu2y3K c5Dezq">R$22,98</div>
                 <div class="lhwDvd Exv9ow B6k-vE">Total de envio:</div>
                 <div class="lhwDvd Uu2y3K B6k-vE">R$15,61</div>
                 <div class="lhwDvd Exv9ow A4gPS6">Pagamento Total:</div>
                 <div class="lhwDvd +0tdvp Uu2y3K A4gPS6">R$38,59</div>
-                <div class="Ql2fz0"><div class="FXKjae"></div><button class="stardust-button stardust-button--primary stardust-button--large gG-FcK">Fazer pedido</button></div>
             </div>
+            <a class="btn btn-lg" style="background-color: #529A4A" href="aguardandopagamento.php?id_camisa=<?php echo $camisa['id_camisa']; ?>" role="button">Fazer pedido</a>
             
         </div>
     </div>
     <script type="text/javascript">
-        let cartao = document.getElementById('cartao')
         let boleto = document.getElementById('boleto')
         let pix = document.getElementById('pix')
-        let htam = ''
-        p.onclick = function(){
-            console.log('Pagamento no cartao de credito')
-            cartao.classList.add("btn-dark");
-            boleto.classList.remove("btn-dark");
-            pix.classList.remove("btn-dark");
-            console.log('mudou')
-            htam = p.innerHTML
-        }
-        m.onclick = function(){
+        let link = document.getElementById('link')
+	    let htam = ''
+        boleto.onclick = function(){
             console.log('Pagamento no boleto bancario')
-            cartao.classList.remove("btn-dark");
-            boleto.classList.add("btn-dark");
-            pix.classList.remove("btn-dark");
-            console.log('mudou')
-            htam = m.innerHTML
+            boleto.classList.add("btn-success");
+            pix.classList.remove("btn-success");
         }
-        g.onclick = function(){
+        pix.onclick = function(){
             console.log('Pagamento PIX')
-            cartao.classList.remove("btn-dark");
-            boleto.classList.remove("btn-dark");
-            pix.classList.add("btn-dark");
-            console.log(g.innerHTML)
-            console.log('mudou')
-            htam = g.innerHTML
+            boleto.classList.remove("btn-success");
+            pix.classList.add("btn-success");
         }
-</script>
+    </script>
 <?php 
 	include "footer.php";
 ?>
--- loja/dadosentrega.php
+++ loja/dadosentrega.php
@@ -1,7 +1,16 @@
 <?php
-    include "conexao.php";
-    if ($_POST['cep'] != '' and $_POST['estado'] != '' and $_POST['cidade'] != '' and $_POST['bairro'] != '' and $_POST['endereco'] != '' and $_POST['numero'] != '' and $_POST['complemento'] != '' and $_POST['salvar como'] != ''){
-        $consulta = "INSERT INTO entrega (cep, estado, cidade, bairro, endereco, numero, complemento, salvar como) VALUES ('{$_POST['cep']}', '{$_POST['estado']}', '{$_POST['cidade']}', '{$_POST['bairro']}', '{$_POST['endereco']}', '{$_POST['numero']}'), '{$_POST['complemento']}', '{$_POST['salvar como']}' )";
-    }
+    $servidor = 'localhost';
+    $usuario = 'root';
+    $senha = '';
+    $banco = 'bancoloja';
+
+    $mysql = mysqli_connect($servidor,$usuario,$senha,$banco);
+
+    $consulta = "INSERT INTO entrega (cep, estado, cidade, bairro, endereco, numero, complemento, salvar como) VALUES ('{$_POST['cep']}', '{$_POST['estado']}', '{$_POST['cidade']}', '{$_POST['bairro']}', '{$_POST['endereco']}', '{$_POST['numero']}'), '{$_POST['complemento']}', '{$_POST['salvar como']}' )";
+
     $incluir = mysqli_query($mysql, $consulta);
+    $novo_id = mysqli_insert_id($mysql);
+    if($incluir){
+        echo "<html><h5 style='text-align:center'>Dados inseridos com sucesso!</h5></html>";
+    }
 ?>
