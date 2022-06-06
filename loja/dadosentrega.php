52f35897b278d4f1b3eb157fa170493310c351b5
Megelado <99930585+Megelado@users.noreply.github.com>
2022-06-06 13:48:56 -0300

    Update dadosentrega.php

--- loja/dadosentrega.php
+++ loja/dadosentrega.php
@@ -1,16 +1,11 @@
+
 <?php
-    $servidor = 'localhost';
-    $usuario = 'root';
-    $senha = '';
-    $banco = 'bancoloja';
-
-    $mysql = mysqli_connect($servidor,$usuario,$senha,$banco);
-
-    $consulta = "INSERT INTO entrega (cep, estado, cidade, bairro, endereco, numero, complemento, salvar como) VALUES ('{$_POST['cep']}', '{$_POST['estado']}', '{$_POST['cidade']}', '{$_POST['bairro']}', '{$_POST['endereco']}', '{$_POST['numero']}'), '{$_POST['complemento']}', '{$_POST['salvar como']}' )";
-
+include "conexao.php";
+    $id = $_GET['id_carrinho'];
+    $consulta = "INSERT INTO entrega (`cep`, `estado`, `cidade`, `bairro`, `endereco`, `numero`, `complemento`) VALUES ('{$_POST['cep']}', '{$_POST['estado']}', '{$_POST['cidade']}', '{$_POST['bairro']}', '{$_POST['endereco']}', {$_POST['numero']}, '{$_POST['complemento']}')";
     $incluir = mysqli_query($mysql, $consulta);
-    $novo_id = mysqli_insert_id($mysql);
-    if($incluir){
-       header('Location: fimdecompra.php?sucess=2&id_carrinho='.{$id}.'');
-    }
+    var_dump($incluir);
+    if($incluir != NULL){
+     header('Location: fimdecompra.php?sucess=2&id_carrinho='.$id.'');
+    }else{header('Location: fimdecompra?error=1&id_carrinho='.$id.'');}
 ?>
